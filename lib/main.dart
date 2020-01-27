0183f8f10cadf9c7003f3251b5f3f3239172acb3
WangTung <sparky800321@gmail.com>
2020-01-27 16:58:12 +0800

    Add: 新增tung.jpg
    Update: 增加Column(內含Circle acatar及Text)

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/tung.jpg'),
+              ),
+              Text(
+                'Tung Wang',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
