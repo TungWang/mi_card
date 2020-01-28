d5b1941afa0fc730ea139a8b2dbdaaa85225074e
WangTung <sparky800321@gmail.com>
2020-01-28 19:42:57 +0800

    Add: fonts
    Update: 新增電話號碼及電子郵件資訊

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,14 +15,85 @@ class MyApp extends StatelessWidget {
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
-                backgroundImage: AssetImage('images/tung.jpg'),
+                backgroundImage: AssetImage(
+                  'images/tung.jpg',
+                ),
               ),
               Text(
                 'Tung Wang',
                 style: TextStyle(
+                  fontFamily: 'PermanentMarker',
                   fontSize: 40.0,
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'BebasNeue',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                padding: EdgeInsets.all(
+                  10.0
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      '+886 0928 825 720',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'BebasNeue',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                padding: EdgeInsets.all(
+                    10.0
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      'sparky800321@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'BebasNeue',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
                 ),
               )
             ],
@@ -34,4 +103,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
